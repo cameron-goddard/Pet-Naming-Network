f9cba8306762f576a356a4a6b94705501c263d87
map439 <map439@cornell.edu>
2021-12-03 02:14:52 -0500

    Tweaked SuccessVC

--- Pet Naming Network/SuccessViewController.swift
+++ Pet Naming Network/SuccessViewController.swift
@@ -11,34 +11,61 @@ class SuccessViewController: UIViewController {
 
     
     private var successImageView = UIImageView();
-    
+    private var successLabel1 = UILabel();
+    private var successLabel2 = UILabel();
+    private var closeButton = UIButton(type: .close)
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .white
+        view.backgroundColor = UIColor(red: 247/255, green: 247/255, blue: 247/255, alpha: 1)
         successImageView.image = UIImage(named: "success")
         successImageView.translatesAutoresizingMaskIntoConstraints = false;
         view.addSubview(successImageView)
+        
+        successLabel1.text = "Your Pet Has"
+        successLabel1.textAlignment = .center
+        successLabel1.font = UIFont.boldSystemFont(ofSize: 24)
+        successLabel1.textColor = .orange
+        successLabel1.translatesAutoresizingMaskIntoConstraints = false;
+        view.addSubview(successLabel1)
+        successLabel2.text = "Been Successfully Uploaded!"
+        successLabel2.textAlignment = .center
+        successLabel2.font = UIFont.boldSystemFont(ofSize: 24)
+        successLabel2.textColor = .orange
+        successLabel2.translatesAutoresizingMaskIntoConstraints = false;
+        view.addSubview(successLabel2)
+        
+        closeButton.addTarget(self, action: #selector(closeView), for: .touchUpInside)
+        closeButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(closeButton)
+        
         setupConstraints()
     }
     
     func setupConstraints(){
+        
+        NSLayoutConstraint.activate([
+            closeButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 15),
+            closeButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15)
+        ])
+        
         NSLayoutConstraint.activate([
             successImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             successImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             successImageView.heightAnchor.constraint(equalToConstant: view.frame.size.width),
             successImageView.widthAnchor.constraint(equalToConstant: view.frame.size.width)
         ])
+        NSLayoutConstraint.activate([
+            successLabel1.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            successLabel1.topAnchor.constraint(equalTo: successImageView.bottomAnchor,constant: 40)
+        ])
+        NSLayoutConstraint.activate([
+            successLabel2.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            successLabel2.topAnchor.constraint(equalTo: successLabel1.bottomAnchor,constant: 8)
+        ])
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @objc func closeView() {
+        dismiss(animated: true, completion: nil)
     }
-    */
-
 }
