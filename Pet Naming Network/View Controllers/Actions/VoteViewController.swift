2a81f1d5348e4fc96aa881a2a23710b702d24cfe
Cameron Goddard <camerongoddard@me.com>
2021-12-03 01:47:50 -0500

    Add voting data, buttons, transition animation

--- Pet Naming Network/View Controllers/Actions/VoteViewController.swift
+++ Pet Naming Network/View Controllers/Actions/VoteViewController.swift
@@ -11,15 +11,22 @@ class VoteViewController: UIViewController {
 
     private var imageView = UIImageView()
     private var namesTableView = UITableView()
+    private var skipButton = UIButton()
     let userName = "test"
     
-    private var votableNames : [Pet] = [Pet(petName: "Doggo", user: "test", petImageURL: "doggo", petState: .Featured),Pet(petName: "???", user: "test", petImageURL: "nice", petState: .Featured),Pet(petName: "Gamer", user: "test", petImageURL: "gamer", petState: .Featured),Pet(petName: "cat", user: "test", petImageURL: "waffle", petState: .Featured),Pet(petName: "cat", user: "test", petImageURL: "waffle", petState: .Featured),Pet(petName: "cat", user: "test", petImageURL: "waffle", petState: .Featured),Pet(petName: "cat", user: "test", petImageURL: "waffle", petState: .Featured)]
+    private var votableNames : [Pet] = [Pet(petName: "Doggo", user: "test", petImageURL: "doggo", petState: .FEATURED),Pet(petName: "???", user: "test", petImageURL: "nice", petState: .FEATURED),Pet(petName: "Gamer", user: "test", petImageURL: "gamer", petState: .FEATURED),Pet(petName: "cat", user: "test", petImageURL: "waffle", petState: .FEATURED),Pet(petName: "cat", user: "test", petImageURL: "waffle", petState: .FEATURED)]
     
     private let reuseIdentifier = "votableNameCellReuse"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //view.backgroundColor = .secondarySystemBackground
+        view.backgroundColor = .secondarySystemBackground
+        
+        
+        namesTableView.tableHeaderView = UIView()
+        namesTableView.alwaysBounceVertical = false
         namesTableView.layer.cornerRadius = 10
         namesTableView.delegate = self
         namesTableView.dataSource = self
@@ -32,7 +39,31 @@ class VoteViewController: UIViewController {
         imageView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(imageView)
         
+        skipButton.configuration = .filled()
+        
+        skipButton.configuration?.buttonSize = .large
+        skipButton.setTitle("Skip", for: .normal)
+        skipButton.tag = -1
+        skipButton.addTarget(self, action: #selector(newImage(_:)), for: .touchUpInside)
+        skipButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(skipButton)
+        
         setUpConstraints()
+    }
+    
+    @objc func newImage(_ sender: UIButton) {
+        if sender.tag != -1 {
+            
+        }
+        
+        UIView.transition(with: imageView, duration: 0.5, options: .transitionCrossDissolve, animations: {
+            self.imageView.image = UIImage(systemName: "bolt.ring.closed")
+        
+        }, completion: nil)
+        UIView.transition(with: namesTableView, duration: 0.5, options: .transitionCrossDissolve, animations: {
+            self.votableNames = [Pet(petName: "Doggo", user: "test", petImageURL: "doggo", petState: .FEATURED),Pet(petName: "???", user: "test", petImageURL: "nice", petState: .FEATURED),Pet(petName: "Gamer", user: "test", petImageURL: "gamer", petState: .FEATURED),Pet(petName: "cat", user: "test", petImageURL: "waffle", petState: .FEATURED),Pet(petName: "cat", user: "test", petImageURL: "waffle", petState: .FEATURED)]
+            self.namesTableView.reloadData()
+        }, completion: nil)
     }
     
     func setUpConstraints() {
@@ -45,11 +76,15 @@ class VoteViewController: UIViewController {
         NSLayoutConstraint.activate([
             namesTableView.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 20),
             namesTableView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            namesTableView.widthAnchor.constraint(equalToConstant: view.frame.width-20),
-            namesTableView.heightAnchor.constraint(equalToConstant: 500)
+            namesTableView.widthAnchor.constraint(equalToConstant: view.frame.width-40),
+            namesTableView.heightAnchor.constraint(equalToConstant: CGFloat(votableNames.count*60))
+        ])
+        NSLayoutConstraint.activate([
+            skipButton.topAnchor.constraint(equalTo: namesTableView.bottomAnchor, constant: 20),
+            skipButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            skipButton.widthAnchor.constraint(equalToConstant: view.frame.width-40)
         ])
     }
-    
 
     /*
     // MARK: - Navigation
@@ -60,6 +95,8 @@ class VoteViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    
 
 }
 
@@ -71,7 +108,10 @@ extension VoteViewController: UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as? VotableNameTableViewCell {
-            
+            cell.voteButton.addTarget(self, action: #selector(newImage(_:)), for: .touchUpInside)
+            cell.voteButton.tag = indexPath.row
+            cell.dislikeButton.addTarget(self, action: #selector(hideName(_:)), for: .touchUpInside)
+            cell.dislikeButton.tag = indexPath.row
             let pet = votableNames[indexPath.row]
             cell.configure(name: pet.petName)
             return cell
@@ -79,12 +119,19 @@ extension VoteViewController: UITableViewDataSource {
             return UITableViewCell()
         }
     }
+    
+    @objc func hideName(_ sender: UIButton) {
+        votableNames.remove(at: sender.tag)
+        namesTableView.deleteRows(at: [IndexPath(row: sender.tag, section: 0)], with: .fade)
+        namesTableView.reloadData()
+        //setUpConstraints()
+    }
 }
 
 
 extension VoteViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 45
+        return 60
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let song = votableNames[indexPath.row]
--- Pet Naming Network/VotableNameTableViewCell.swift
+++ Pet Naming Network/VotableNameTableViewCell.swift
@@ -10,15 +10,39 @@ import UIKit
 class VotableNameTableViewCell: UITableViewCell {
 
     private var nameLabel = UILabel()
+    var voteButton = UIButton()
+    var dislikeButton = UIButton()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        backgroundColor = .blue
+        
         nameLabel.font = .systemFont(ofSize: 24, weight: .bold)
         nameLabel.translatesAutoresizingMaskIntoConstraints = false
         contentView.addSubview(nameLabel)
         
+        voteButton.configuration = .tinted()
+        voteButton.setTitle("Vote", for: .normal)
+        voteButton.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(voteButton)
+        
+        dislikeButton.configuration = .tinted()
+        dislikeButton.configuration?.baseBackgroundColor = .systemRed
+        dislikeButton.configuration?.baseForegroundColor = .systemRed
+        //dislikeButton.addTarget(self, action: #selector(whiteOut), for: .touchUpInside)
+        dislikeButton.configuration?.image = UIImage(systemName: "hand.thumbsdown")
+        dislikeButton.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(dislikeButton)
+        
+        
+        
         setUpConstraints()
+    }
+    
+    @objc func whiteOut() {
+        //print("here")
+        //self.tablev
+        //self.isOpaque = true
+        //contentView.willRemoveSubview(<#T##subview: UIView##UIView#>)
     }
     
     
@@ -28,8 +52,16 @@ class VotableNameTableViewCell: UITableViewCell {
     
     func setUpConstraints() {
         NSLayoutConstraint.activate([
-            nameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
-            nameLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 5)
+            nameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            nameLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ])
+        NSLayoutConstraint.activate([
+            voteButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
+            voteButton.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ])
+        NSLayoutConstraint.activate([
+            dislikeButton.trailingAnchor.constraint(equalTo: voteButton.leadingAnchor, constant: -10),
+            dislikeButton.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
         ])
     }
     
